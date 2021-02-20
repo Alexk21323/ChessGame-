e955ff87b95b9f1eaf84fac6b6876596658dec05
Park-Ryan <ryankpark23@gmail.com>
2021-02-20 02:25:17 -0800

    First commit of the piece classes

---
+++ piece.h
@@ -0,0 +1,20 @@
+#ifndef _PIECES_H
+#define _PIECES_H
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Pieces{
+
+
+
+};
+
+
+
+
+
+
+
+
+#endif //_PIECES_H
---
+++ pieceSprite.cpp
@@ -0,0 +1,1 @@
+#include "pieceSprite.h"
