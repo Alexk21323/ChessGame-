5a3808f4040d2d77959b47f47a7d843936f48b00
Park-Ryan <ryankpark23@gmail.com>
2021-03-07 01:35:19 -0800

    cleaned up file

--- human.hpp
+++ human.hpp
@@ -13,37 +13,31 @@ public:
     int yPiece;
     bool select(int x, int y, Board *board)
     {
-        this->xPiece = x/100;
-        this->yPiece = y/100;
-        int tmpx = x/100;
+        this->xPiece = x / 100;
+        this->yPiece = y / 100;
+        int tmpx = x / 100;
         int tmpy = y / 100;
 
-         if(board->getGrid({xPiece, yPiece})->getPiece()!= nullptr){
+        if (board->getGrid({xPiece, yPiece})->getPiece() != nullptr)
+        {
+            return true;
+        }
 
-             return true;
-         }
-         
-        //get the pixel position to coordinates
-
-       // std::cout << "initial: " << tmpx << "\t" << tmpy << "\n";
-        // board->getGrid({7,7})->getPiece()->setPosition(tmpx,tmpy);
         return false;
     }
     bool makeMove(int mouseX, int mouseY, Board *board)
     {
         this->x = mouseX;
         this->y = mouseY;
-        
         int destinationX = x / 100;
         int destinationY = y / 100;
-        //std::cout << "tmp coord: " << x << "\t" << y << "\n";
-        //std::cout << "final: " << destinationX << "\t" << destinationY << "\n";
         Piece *tmpPiece;
-        if(board->getGrid({xPiece, yPiece})->getPiece()!= nullptr){
-        tmpPiece = board->getGrid({xPiece, yPiece})->getPiece();
-        tmpPiece->setPosition(destinationX, destinationY);
-        board->setPiece(tmpPiece);
-        return true;
+        if (board->getGrid({xPiece, yPiece})->getPiece() != nullptr)
+        {
+            tmpPiece = board->getGrid({xPiece, yPiece})->getPiece();
+            tmpPiece->setPosition(destinationX, destinationY);
+            board->setPiece(tmpPiece);
+            return true;
         }
         return false;
     }
